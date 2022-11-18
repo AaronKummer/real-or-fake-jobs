c759d40c0ba0a4f15daaa82bb9f0478fbcaf6a47
Aaron Kummer <aaronjkummer@gmail.com>
2022-11-17 19:25:16 -0500

    only makes predictions based on test data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot
 from sklearn import metrics, model_selection
+from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from InputHelper import InputHelper
@@ -11,8 +12,6 @@ from InputHelper import InputHelper
 
 def main():
     print('starting program')
-    # get some input
-    ih = InputHelper()
     # parse csv
     df = pd.read_csv('fake_job_postings.csv')
     # wrangle data
@@ -30,41 +29,47 @@ def main():
     X = df[['telecommuting', 'has_company_logo',
             'has_questions', 'employment_type', 'is_in_usa']]
     y = df['fraudulent']
-    input = [ih.remote, ih.has_logo, ih.has_logo,
-             ih.employment_type, ih.is_in_usa]
-    input = np.array(input)
-    input = input.reshape(1, -1)
 
     # evaluate accuracy
-    training_model = DecisionTreeClassifier()
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X, y, test_size=0.9)
-    training_model.fit(X_train, y_train)
-    v_pred = training_model.predict(X_test)
-    accuracy = metrics.accuracy_score(y_test, v_pred)
-
     model = DecisionTreeClassifier()
-    model.fit(X, y)
-    y_pred = model.predict(input)
-    answer = 'fake' if y_pred == 1 else 'real'
-    print('the job is ' + str(round(accuracy*100, 2)) + '% likely to be ' + answer)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y,test_size=.8)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    print(accuracy)
+
+    # get user input
+    # input = [ih.remote, ih.has_logo, ih.has_logo,
+    #          ih.employment_type, ih.is_in_usa]
+    # input = np.array(input)
+    # input = input.reshape(1, -1)
+    # ih = InputHelper()
+
+    # answer = 'fake' if y_pred == 1 else 'real'
+    # print(answer)
+    # print('the job is ' + str(round(accuracy*100, 2)) + '% likely to be ' + answer)
 
     # visualize data
-    Z = df[['fraudulent','telecommuting', 'has_company_logo',
-            'has_questions', 'employment_type', 'is_in_usa']]
-    Z['fraudulent'] = Z['fraudulent'].astype(bool)
-    Z['telecommuting'] = Z['telecommuting'].astype(bool)
-    Z['has_company_logo'] = Z['has_company_logo'].astype(bool)
-    Z['has_questions'] = Z['has_questions'].astype(bool)
-    Z['is_in_usa'] = Z['is_in_usa'].astype(bool)
     sns.set()
-    sns.relplot(x='fraudulent',y='telecommuting', col='is_in_usa', data=Z, kind='line')
+    cm = confusion_matrix(y_test,y_pred)
+    sns.heatmap(cm, cmap='Greens',xticklabels=[0,1],yticklabels=[0,1])
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
     plt.show()
-    sns.catplot(x='fraudulent',y='has_company_logo', col='is_in_usa', data=Z, kind='bar')
+    # Z = df[['fraudulent','telecommuting', 'has_company_logo',
+    #         'has_questions', 'employment_type', 'is_in_usa']]
+    # Z['fraudulent'] = Z['fraudulent'].astype(bool)
+    # Z['telecommuting'] = Z['telecommuting'].astype(bool)
+    # Z['has_company_logo'] = Z['has_company_logo'].astype(bool)
+    # Z['has_questions'] = Z['has_questions'].astype(bool)
+    # Z['is_in_usa'] = Z['is_in_usa'].astype(bool)
+    sns.relplot(x='fraudulent',y='telecommuting', col='is_in_usa', data=df, kind='line')
     plt.show()
-    sns.catplot(x='fraudulent',y='has_questions', col='is_in_usa', data=Z, kind='bar')
+    sns.catplot(x='fraudulent',y='has_company_logo', col='is_in_usa', data=df, kind='bar')
     plt.show()
-    print(Z.info())
+    sns.catplot(x='fraudulent',y='has_questions', col='is_in_usa', data=df, kind='bar')
+    plt.show()
 
 def is_in_usa(row):
     if row['location'].split(',')[0] == 'US':
